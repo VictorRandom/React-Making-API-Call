1d24fda9ea6cac1e1c2452b78f8791df25d28a70
VictorRandom <victormbalduino@hotmail.com>
2022-07-18 09:51:33 -0300

    using useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [starWarsData, setStarWarsData] = useState({})
+    
+    // side effects
+    useEffect(function(){
+      console.log("Component rendered")
+      
+      fetch("https://swapi.dev/api/people/1")
+          .then(res => res.json())
+          .then(data => setStarWarsData(data))
+    }, [])
+    
+    return (
+        <div>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+        </div>
+    )
 }
-
-export default App;
