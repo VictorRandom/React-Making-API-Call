81aa49f4ed36ff326255b27c83e00fc6387ca85b
VictorRandom <victormbalduino@hotmail.com>
2022-07-18 10:02:12 -0300

    useEffect dependencies array

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { useState, useEffect } from "react"
 
 export default function App() {
-    const [starWarsData, setStarWarsData] = useState({})
+    const [starWarsData, setStarWarsData] = useState({});
+    const [count, setCount] = useState(0);
     
     // side effects
-    useEffect(function(){
-      console.log("Component rendered")
+    // useEffect(function(){
+    //   console.log("Component rendered")
       
-      fetch("https://swapi.dev/api/people/1")
-          .then(res => res.json())
-          .then(data => setStarWarsData(data))
-    }, [])
+    //   fetch("https://swapi.dev/api/people/1")
+    //       .then(res => res.json())
+    //       .then(data => setStarWarsData(data))
+    // }, [])
     
+
+    useEffect(() => {
+      console.log("Effect function ran");
+    }, [count])
+
     return (
         <div>
             <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+            <h2>The count is {count}</h2>
+            <button onClick={() => setCount(prevCount => prevCount + 1)} >Add</button>
         </div>
     )
 }
